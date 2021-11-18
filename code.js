1c0b65eda6df9089ef873a9761772badb5a3d8ad
gary <gary.white@lodgit.com>
2021-11-18 13:17:17 +0100

    boilerplate

---
+++ code.js
@@ -0,0 +1,7 @@
+var countSheep = function (num){
+    //your code here
+}
+
+console.log(countSheep(1)); // Return "1 sheep..."
+console.log(countSheep(2)); // Return "1 sheep...2 sheep..."
+console.log(countSheep(3)); // Return "1 sheep...2 sheep...3 sheep..."
