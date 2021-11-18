13700fc1f6935c7d491a58d99357368d736c1367
gary <gary.white@lodgit.com>
2021-11-18 16:45:15 +0100

    code semi-working

--- code.js
+++ code.js
@@ -1,5 +1,23 @@
-var countSheep = function (num){
-    //your code here
+// var countSheep = function (num){
+//     //your code here
+//     var myNum = 1;
+//     var myText = myNum + " sheep...";
+//     for (var i = 0; i < num.length; i++){
+//         // myText =  myText + myNum;
+//         myNum = myNum + 1;
+//     }
+//     console.log(myText);
+// }
+
+function countSheep(num){
+    let myNum = 1;
+    let text = " Sheep...";
+    
+    for (var i = 1; i < num; i++) {
+    text = text + (myNum + i) + text;
+        // myNum + text;
+    }
+  console.log(myNum + text);
 }
 
 console.log(countSheep(1)); // Return "1 sheep..."
